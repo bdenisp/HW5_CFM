5df81783cfd523446cfa7150278e2beca4492447
bdenisp <bdenisp78@gmail.com>
2019-11-06 22:50:31 +0400

    Тестирование функций

--- test_python.py
+++ test_python.py
@@ -9,11 +9,21 @@ import math
 
 # filter
 def test_filter():
-    assert True
+    assert list(filter(lambda x: x[1] == 'e', ['leo', 'lao', 'luo', 'lio', 'leo'])) == ['leo', 'leo']
+    assert list(filter(lambda x: x == 'leo', ['leo', 'lao', 'luo', 'lio', 'leo'])) == ['leo', 'leo']
+
 
 # map
+def test_map():
+    assert list(map(lambda x: x.upper(), ['leo', 'lao', 'luo', 'lio', 'leo'])) == ['LEO', 'LAO', 'LUO', 'LIO', 'LEO']
+    assert list(map(int, [1.0, 2.0, 3.0])) == [1, 2, 3]
+
 
 # sorted
+def test_sorted():
+    assert list(sorted(['leo', 'lao', 'luo', 'lio', 'leo'], key=lambda x: x[1])) == ['lao', 'leo', 'leo', 'lio', 'luo']
+    assert list(sorted(['leo', 'lao', 'luo', 'lio', 'leo'], key=lambda x: x[1], reverse=True)) == ['luo', 'lio', 'leo',
+                                                                                                   'leo', 'lao']
 
 
 # pi from math
@@ -23,9 +33,9 @@ def test_pi():
 
 # sqrt from math
 def test_sqrt():
-    assert (math.sqrt(4) ** 2 - 4) < 0.00000001
-    assert (math.sqrt(5) ** 2 - 5) < 0.00000001
-    assert (math.sqrt(6) ** 2 - 6) < 0.00000001
+    assert (math.sqrt(4) ** 2 - 4) < 0.00000000001
+    assert (math.sqrt(5) ** 2 - 5) < 0.00000000001
+    assert (math.sqrt(6) ** 2 - 6) < 0.00000000001
 
 
 # pow from math
@@ -40,4 +50,3 @@ def test_hypot():
     assert (math.hypot(2, 3) ** 2 - 13) < 0.00000000001
     assert (math.hypot(3, 4) ** 2 - 25) < 0.00000000001
     assert (math.hypot(4, 5) ** 2 - 41) < 0.00000000001
-
