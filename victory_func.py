ce3c3ae39a25f340669f8e7dbe1ce944ea490cca
bdenisp <bdenisp78@gmail.com>
2019-11-28 10:14:36 +0400

    Добавление работы с файлами.
    Сохранение истории в подпрограмме "Мой банковский счет".
    
    Добавление функционала в подпрограмму "Консольный файловый менеджер".
    Сохранение содержимого рабочей директории в файл.
    
    Использование тернарных операторов
    Использование обработки исключений
    Добавлено использование декоратора

--- victory_func.py
+++ victory_func.py
@@ -88,6 +88,32 @@ def trasformation_date(date_arg, month_arg, year_arg):
     return ('{} {} {} года'.format(day_famous.get(date_arg), month_famous.get(month_arg), year_arg))
 
 
+# декоратор
+def add_separator(f):
+    def inner_func(*args, **kwargs):
+        print('/' * 30)
+        res = f(*args, **kwargs)
+        print('/' * 30)
+        return res
+
+    return inner_func
+
+
+# применение декоратора
+@add_separator
+def print_stat(arg_all, arg_true):
+    arg_all = int(arg_all)
+    arg_true = int(arg_true)
+    print("Статистика")
+    print("Всего вопросов:", arg_all)
+    print("Правильных ответов:", arg_true)
+    print("Ошибок:", arg_all - arg_true)
+    try:
+        print("Процент правильных ответов:", 100 * arg_true / arg_all, '%')
+    except ZeroDivisionError:
+        print('Результат может Вас испугать, т.к. в знаменателе 0!')
+
+
 def victory():
     bullvictory = True
     number_question = 0
@@ -122,16 +148,8 @@ def victory():
                 print('{} родился {}'.format(keys_famous[number_famous],
                                              trasformation_date(date_true, month_true, year_true)))
 
-        print("//////////////////////////")
-        print("Статистика")
-        print("Всего вопросов:", countall)
-        print("Правильных ответов:", counttrue)
-        print("Ошибок:", countall - counttrue)
-        try:
-            print("Процент правильных ответов:", 100 * counttrue / countall, '%')
-        except ZeroDivisionError:
-            print('Результат может Вас испугать, т.к. в знаменателе 0!')
-        print("//////////////////////////")
+        #вызов декорируемой функции
+        print_stat(countall, counttrue)
 
         bullvictory = input('Желаете еще раз сыграть?(y=Да/Прочее = нет)')
 
